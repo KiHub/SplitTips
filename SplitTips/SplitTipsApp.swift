d14bfd43ce943a60901cba31dea4de461f59ebfa
Kirill Ki <kirillkalapov@gmail.com>
2022-06-02 11:57:59 +0200

    Initial Commit

---
+++ SplitTips/SplitTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTipsApp.swift
+//  SplitTips
+//
+//  Created by  Mr.Ki on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SplitTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
