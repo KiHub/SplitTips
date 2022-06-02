6e43cb653526899a64374d27092ca387c5493016
Kirill Ki <kirillkalapov@gmail.com>
2022-06-02 12:34:39 +0200

    Test swift ui

--- SplitTips/ContentView.swift
+++ SplitTips/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeole = 2
+    @State private var tipPercentage = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeole + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                    Picker(selection: $numberOfPeole) {
+                        ForEach(2..<50) {
+                            Text("\($0) people")
+                        }
+                    } label: {
+                        Text("Number of people")
+                    }
+                    
+                }
+                Section {
+                    Picker(selection: $tipPercentage, content: {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }, label: { Text("Tip percentage")})
+                } header: {
+                    Text("How many tips?")
+                }
+                .pickerStyle(.segmented)
+                Section {
+                    
+                    Text("\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationTitle("Split tips")
+            
+            
+        }
+        
+        
     }
 }
 
